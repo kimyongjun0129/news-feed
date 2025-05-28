a23c6c4db06cd680e76b8fd236dfe0498eb547c5
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-28 11:06:33 +0900

    feat: BaseEntity, Member 클래스 추가

--- src/main/java/org/example/newsfeed/common/entity/Entity.java
+++
@@ -1,4 +0,0 @@
-package org.example.newsfeed.common.entity;
-
-public class Entity {
-}
---
+++ src/main/java/org/example/newsfeed/member/entity/Member.java
@@ -0,0 +1,32 @@
+package org.example.newsfeed.member.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.example.newsfeed.common.entity.BaseEntity;
+
+@Getter
+@NoArgsConstructor
+@Entity
+@Table(name = "members")
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "member_name", nullable = false)
+    private String memberName;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public Member(String memberName, String email, String password) {
+        this.memberName = memberName;
+        this.email = email;
+        this.password = password;
+    }
+}
