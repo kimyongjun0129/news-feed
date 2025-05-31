c9374c5f33f6b7bca36e70eabc7f76250aa20cf9
kimyongjun0129 <kimyongjun0129@naver.com>
2025-05-31 23:24:03 +0900

    refactor: @AuthenticationPrincipal 형태로 user 정보 받게 수정

--- src/main/java/org/example/newsfeed/follow/controller/FollowController.java
+++ src/main/java/org/example/newsfeed/follow/controller/FollowController.java
@@ -1,12 +1,14 @@
 package org.example.newsfeed.follow.controller;
 
+import org.example.newsfeed.common.dto.AuthUser;
 import org.example.newsfeed.follow.service.FollowService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/api/members")
 public class FollowController {
     private final FollowService followService;
 
@@ -17,9 +19,9 @@ public class FollowController {
     @PostMapping("/{id}/follow")
     public ResponseEntity<Void> followUser(
             @PathVariable("id") Long followerId,
-            @RequestAttribute("memberId") Long followeeId
+            @AuthenticationPrincipal AuthUser authUser
     ) {
-        followService.followUser(followeeId, followerId);
+        followService.followUser(authUser.getId(), followerId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
