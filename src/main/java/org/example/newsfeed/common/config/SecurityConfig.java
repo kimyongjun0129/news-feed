761fa510b61c71c5eb6947b5205ea638645fd4ca
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-01 19:36:57 +0900

    fix: 경로 틀림으로써 생기는 오류 해결

--- src/main/java/org/example/newsfeed/common/config/SecurityConfig.java
+++ src/main/java/org/example/newsfeed/common/config/SecurityConfig.java
@@ -46,7 +46,7 @@ public class SecurityConfig {
 
                         // 게시물 (post)
                         .requestMatchers(HttpMethod.POST, "/api/posts").hasRole(UserRole.ADMIN.name())
-                        .requestMatchers(HttpMethod.GET, "/api/posts").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/posts/*").permitAll()
                         .requestMatchers(HttpMethod.GET, "/api/posts/*").permitAll()
                         .requestMatchers(HttpMethod.PATCH, "/api/posts/*").hasRole(UserRole.ADMIN.name())
                         .requestMatchers(HttpMethod.DELETE, "/api/posts/*").hasRole(UserRole.ADMIN.name())
