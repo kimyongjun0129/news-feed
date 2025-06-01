e3c52e5b0b994552b24506e52d708a97e945e567
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-01 12:14:00 +0900

    docs: 팔로우, 언팔로우에 대한 주석 추가

--- src/main/java/org/example/newsfeed/follow/controller/FollowController.java
+++ src/main/java/org/example/newsfeed/follow/controller/FollowController.java
@@ -16,6 +16,12 @@ public class FollowController {
         this.followService = followService;
     }
 
+    /**
+     * @author : kimyongjun0129
+     * @param   followerId   팔로우 걸고 싶은 사람의 PK 값
+     * @param   authUser     현재 로그인된 사용자 객체 (Spring Security)
+     * @return               상태코드 반환
+     */
     @PostMapping("/{id}/follow")
     public ResponseEntity<Void> followUser(
             @PathVariable("id") Long followerId,
@@ -25,11 +31,16 @@ public class FollowController {
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    /**
+     * @author : kimyongjun0129
+     * @param   unFollowerId    팔로우 걸고 싶은 사람의 PK 값
+     * @return                  상태코드 반환
+     */
     @PostMapping("/{id}/unfollow")
     public ResponseEntity<Void> unfollowUser(
-            @PathVariable("id") Long id
+            @PathVariable("id") Long unFollowerId
     ) {
-        followService.unfollowUser(id);
+        followService.unfollowUser(unFollowerId);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 }
