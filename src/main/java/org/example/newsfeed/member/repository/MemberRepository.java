2c84df7ade19109318889d58e270d1727ab54f33
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-02 10:45:47 +0900

    remove: profileRepository 변경 및 삭제

--- src/main/java/org/example/newsfeed/member/repository/MemberRepository.java
+++ src/main/java/org/example/newsfeed/member/repository/MemberRepository.java
@@ -4,7 +4,9 @@ import org.example.newsfeed.common.exception.CustomException;
 import org.example.newsfeed.common.exception.error.CustomErrorCode;
 import org.example.newsfeed.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -23,5 +25,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     }
 
     Member getMemberByEmail(String email);
+
+    //createuser
+    default Member findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Does not exist id + "+ id));
+    }
+
 }
 
