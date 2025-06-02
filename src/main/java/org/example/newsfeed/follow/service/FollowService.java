ef201ea6695ef1d1b3b66ec9abfc9b9afdd1c4d4
kimyongjun0129 <kimyongjun0129@naver.com>
2025-06-02 19:32:57 +0900

    docs: 팔로우, 언팔로우 메서드 자바 docs 추가

--- src/main/java/org/example/newsfeed/follow/service/FollowService.java
+++ src/main/java/org/example/newsfeed/follow/service/FollowService.java
@@ -21,6 +21,27 @@ public class FollowService {
     private final FollowRepository followRepository;
     private final MemberRepository memberRepository;
 
+    /**
+     * 팔로이(followeeId)가 팔로워(followerId)를 팔로우하도록 처리합니다.
+     * <p>
+     * 이 메서드는 다음과 같은 검증을 수행합니다:
+     * <ul>
+     *   <li>자기 자신을 팔로우하려는 경우 예외를 발생시킵니다.</li>
+     *   <li>팔로워 또는 팔로이 유저가 존재하지 않는 경우 예외를 발생시킵니다.</li>
+     *   <li>이미 팔로우한 유저를 다시 팔로우하려는 경우 예외를 발생시킵니다.</li>
+     * </ul>
+     * 모든 검증을 통과하면 새로운 {@link Follow} 엔티티를 생성하여 저장합니다.
+     * </p>
+     *
+     * @param followeeId 팔로우 당할 유저의 ID
+     * @param followerId 팔로우를 시도하는 유저의 ID
+     * @throws CustomException 다음과 같은 경우 예외가 발생합니다:
+     *                         <ul>
+     *                           <li>자기 자신을 팔로우하려는 경우 (SELF_FOLLOW_NOT_ALLOWED)</li>
+     *                           <li>유저가 존재하지 않는 경우 (USER_NOT_FOUND)</li>
+     *                           <li>이미 팔로우 관계가 존재하는 경우 (FOLLOW_ALREADY_EXISTS)</li>
+     *                         </ul>
+     */
     @Transactional
     public void followUser(Long followeeId, Long followerId) {
 
@@ -47,6 +68,28 @@ public class FollowService {
         followRepository.save(follows);
     }
 
+    /**
+     * 사용자가 팔로우하고 있던 유저를 언팔로우 처리합니다.
+     *
+     * <p>
+     * 이 메서드는 다음과 같은 검증을 수행합니다:
+     * <ul>
+     *   <li>자기 자신을 언팔로우하려는 경우 예외 발생</li>
+     *   <li>언팔로우 시도하는 사용자가 존재하지 않을 경우 예외 발생</li>
+     *   <li>실제로 팔로우 관계가 존재하지 않을 경우 예외 발생</li>
+     * </ul>
+     * 검증을 통과하면 {@link Follow} 엔티티를 삭제하여 언팔로우 처리를 완료합니다.
+     * </p>
+     *
+     * @param followeeId 언팔로우 당할 유저의 ID
+     * @param unFollowerId 언팔로우를 시도하는 유저의 ID
+     * @throws CustomException 다음의 경우 예외 발생:
+     * <ul>
+     *   <li>자기 자신을 언팔로우하려는 경우 (SELF_UNFOLLOW_NOT_ALLOWED)</li>
+     *   <li>언팔로우를 시도하는 유저가 존재하지 않을 경우 (USER_NOT_FOUND)</li>
+     *   <li>팔로우 관계가 존재하지 않을 경우 (FOLLOW_NOT_FOUND)</li>
+     * </ul>
+     */
     @Transactional
     public void unfollowUser(Long followeeId, Long unFollowerId) {
 
